97481aacece058243c55038e4fcc099ff6a9c1bd
Justin Ke <keyuqing@gmail.com>
2018-09-13 15:27:26 -0400

    new

--- main.js
+++ main.js
@@ -2,3 +2,15 @@
 //----------------------------------
 //          YOUR CODE
 //----------------------------------
+//create(100,100,'black');
+var counter=0;
+var row =0;
+for(var row =0; row<81; row++){
+        y = row*15;
+    for(var col=0; col<53; col++){
+        x= col*15;
+        create(x,y,colors[counter]);
+        counter++;
+    }
+        
+}
